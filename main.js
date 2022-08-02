ee90b1776e4ebc7be9c851f71bc30b91902a99cc
Arturo-Valdez <arturovaldez019@gmail.com>
2022-08-02 18:00:31 -0500

    first commit

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 var grid = [,];
+var output= '';
 
+// Ingreso de datos tabla
 var row = prompt("Ingrese el numero filas");
 var column = prompt("Ingrese el numero columnas");
 
+
+// Bacterias vivas
 var n = prompt("Ingrese el numero de celulas vivas");
 
+
+// Crear tabla
 for (var i = 0; i < column ; i++) {
-    for (var i = 0; i < row ; i++) {
-        grid[i, j] = '.';
+    for (var j = 0; j < row ; j++) {
+        grid[i,j]='.'
     }
 }
 
 for (var i = 0; i < n ; i++) {
-    console.log("Ingrese el vector de la celula" + n)
 
-    const a = Math.floor(Math.random() * 1) 
-    const b = Math.floor(Math.random() * 1) 
+    var a = Math.floor(Math.random() * column) 
+    var b = Math.floor(Math.random() * row) 
     grid[a, b] = '*'
     console.log("A>" + a, "B>" + b)
 }
 
 for (var i = 0; i < column ; i++) {
     for (var j = 0; j < row ; j++) {
-        console.log(grid[i, j])
+        output += (grid[i, j])
     }
-    console.log("\n");
+    output += ("\n");
 }
+console.log(output)
